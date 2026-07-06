2b8d4ae6db8e53a224888889cf919d25001251c2
felixpfu <felix@stendec.io>
2026-07-06 14:08:23 -0300

    Ajusta proxy de producao

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,7 @@ const clientIndexPath = path.join(clientDistPath, "index.html");
 
 const app = express();
 
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(express.json({ limit: "64kb" }));
 
